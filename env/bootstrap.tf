4ffce02ba741cf77f82d7466d5d1e83e7bdd75fd
John Wardrop <johnwardrop@outlook.com>
2019-05-12 08:43:44 +0100

    temp 4

--- env/bootstrap.tf
+++ env/bootstrap.tf
@@ -17,7 +17,7 @@ output "vault_uri" {
 data "azurerm_key_vault_secret" "adm-usr-server2016-prd" {
   name      = "adm-usr-server2016-prd"
   key_vault_id = "${data.azurerm_key_vault.kv-dev-core.id}"
-  depends_on = ["data.azurerm_key_vault.kv-dev-core"]
+  depends_on = ["vault_uri"]
 }
 
 data "azurerm_key_vault_secret" "sec-dsc-ep" {
