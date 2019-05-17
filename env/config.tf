1d92e64b4567525c96a34940604ce2d4970f1483
John Wardrop <jwardrop85@gmail.com>
2019-05-17 10:04:30 +0100

    provider config jenkins

--- env/config.tf
+++ env/config.tf
@@ -7,6 +7,24 @@ terraform {
   }
 }
 
+
+variable "subs_id"{
+  
+}
+variable "client_id"{
+  
+}
+variable "client_secret"{
+  
+}
+variable "tenant_id"{
+  
+}
+
 provider "azurerm" {
   version                                 = "1.27.1"
+  subscription_id = "${var.subs_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
